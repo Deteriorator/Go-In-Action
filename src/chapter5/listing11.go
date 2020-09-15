ad6eaf0fbc2523d77b1d4fe99d622197bf629b24
Leon <Liangz.org@gmail.com>
2020-09-15 22:07:54 +0800

    feat: add listing11.go in chapter5

---
+++ src/chapter5/listing11.go
@@ -0,0 +1,35 @@
+/*
+ * @program: Go-In-Action
+ * @author: Leon
+ * @create: 2020-09-15 21:57
+ */
+package main
+
+import "fmt"
+
+type user struct {
+	name  string
+	email string
+}
+
+func (u user) notify() {
+	fmt.Printf("Sending User Email To %s<%s>\n", u.name, u.email)
+}
+
+func (u *user) changeEmail(email string) {
+	u.email = email
+}
+
+func main() {
+	bill := user{"Bill", "bill@email.com"}
+	bill.notify()
+
+	lisa := &user{"Lisa", "lisa@email.com"}
+	lisa.notify()
+
+	bill.changeEmail("bill@newdomain.com")
+	bill.notify()
+
+	lisa.changeEmail("bill@newdomain.com")
+	lisa.notify()
+}
