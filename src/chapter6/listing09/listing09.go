dd5a77d074e53b94002f3996d017c7472c84ab71
Leon <Liangz.org@gmail.com>
2020-09-16 22:07:35 +0800

    feat: add listing09.go chapter6

---
+++ src/chapter6/listing09/listing09.go
@@ -0,0 +1,39 @@
+/*
+ * @program: Go-In-Action
+ * @author: Leon
+ * @create: 2020-09-16 21:59
+ */
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	counter int
+	wg      sync.WaitGroup
+)
+
+func main() {
+	wg.Add(2)
+
+	go incCounter(1)
+	go incCounter(2)
+
+	wg.Wait()
+	fmt.Println("Final Counter:", counter)
+}
+
+func incCounter(id int) {
+	defer wg.Done()
+
+	for count := 0; count < 2; count++ {
+		value := counter
+		runtime.Gosched()
+
+		value++
+		counter = value
+	}
+}
