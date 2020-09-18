c3213d84e71f407f1599731e23027fc463feb14c
Leon <Liangz.org@gmail.com>
2020-09-18 23:16:54 +0800

    feat: add work chapter7

---
+++ src/chapter7/work/main/main.go
@@ -0,0 +1,50 @@
+/*
+ * @program: Go-In-Action
+ * @author: Leon
+ * @create: 2020-09-18 23:05
+ */
+package main
+
+import (
+	"chapter7/work"
+	"log"
+	"sync"
+	"time"
+)
+
+var names = []string{
+	"steve",
+	"bob",
+	"mary",
+	"therese",
+	"jason",
+}
+
+type namePrinter struct {
+	name string
+}
+
+func (m *namePrinter) Task() {
+	log.Println(m.name)
+	time.Sleep(time.Second)
+}
+
+func main() {
+	p := work.New(5)
+	var wg sync.WaitGroup
+	wg.Add(100 * len(names))
+
+	for i := 0; i < 100; i++ {
+		for _, name := range names {
+			np := namePrinter{
+				name: name,
+			}
+			go func() {
+				p.Run(&np)
+				wg.Done()
+			}()
+		}
+	}
+	wg.Wait()
+	p.Shutdown()
+}
---
+++ src/chapter7/work/work.go
@@ -0,0 +1,43 @@
+/*
+ * @program: Go-In-Action
+ * @author: Leon
+ * @create: 2020-09-18 22:59
+ */
+package work
+
+import "sync"
+
+type Worker interface {
+	Task()
+}
+
+type Pool struct {
+	work chan Worker
+	wg   sync.WaitGroup
+}
+
+func New(maxGoroutines int) *Pool {
+	p := Pool{
+		work: make(chan Worker),
+	}
+
+	p.wg.Add(maxGoroutines)
+	for i := 0; i < maxGoroutines; i++ {
+		go func() {
+			for w := range p.work {
+				w.Task()
+			}
+			p.wg.Done()
+		}()
+	}
+	return &p
+}
+
+func (p *Pool) Run(w Worker) {
+	p.work <- w
+}
+
+func (p *Pool) Shutdown() {
+	close(p.work)
+	p.wg.Wait()
+}
